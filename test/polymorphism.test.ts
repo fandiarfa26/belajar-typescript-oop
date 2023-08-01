ea804b1ec4073a388b11e0b585a3b5b77c9cf020
M. Fandi Arfabuma <arfabuma.987@gmail.com>
2023-08-01 20:04:47 +0700

    type cast

--- test/polymorphism.test.ts
+++ test/polymorphism.test.ts
@@ -8,7 +8,32 @@ describe("Polymorphism", () => {
   class VicePresident extends Manager {}
 
   function sayHello(employee: Employee) {
-    console.info(`Hello ${employee.name}`);
+    if (employee instanceof VicePresident) {
+      const vp = employee as VicePresident;
+      console.info(`Hello VP ${vp.name}`);
+    } else if (employee instanceof Manager) {
+      const manager = employee as Manager;
+      console.info(`Hello Manager ${manager.name}`);
+    } else {
+      console.info(`Hello Employee ${employee.name}`);
+    }
+
+    /**
+     * Pengecekan instanceof harus dimulai dari class turunan terakhir
+     * karena instanceof bernilai true jika class tersebut juga turunannya
+     */
+  }
+
+  function sayHelloWrong(employee: Employee) {
+    if (employee instanceof Manager) {
+      const manager = employee as Manager;
+      console.info(`Hello Manager ${manager.name}`);
+    } else if (employee instanceof VicePresident) {
+      const vp = employee as VicePresident;
+      console.info(`Hello VP ${vp.name}`);
+    } else {
+      console.info(`Hello Employee ${employee.name}`);
+    }
   }
 
   it("should support", () => {
@@ -27,7 +52,13 @@ describe("Polymorphism", () => {
 
   it("should support method parameter polymorphism", () => {
     sayHello(new Employee("Fandi"));
-    sayHello(new Manager("Fandi"));
-    sayHello(new VicePresident("Fandi"));
+    sayHello(new Manager("Arfa"));
+    sayHello(new VicePresident("Buma"));
+  });
+
+  it("should support method parameter polymorphism wrong", () => {
+    sayHelloWrong(new Employee("Fandi"));
+    sayHelloWrong(new Manager("Arfa"));
+    sayHelloWrong(new VicePresident("Buma")); // ini akan dianggap sbg Manager
   });
 });
